cdfde8bf6e9190662837299d753600545ee40c78
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-17 17:35:08 +0200

    :arrow_up: Added Pokemon class for each pokemon

--- Page.js
+++ Page.js
@@ -11,20 +11,20 @@ class Page extends Component {
   addHeader() {
     const header = `<div class="title">
     <h1 class="main-title">POKEMON</h1>
-    <img src="https://www.clipartmax.com/png/middle/71-710762_gengar-decal-by-mute-owl-pokemon-silhouette.png" alt="pokemon silhouette" className="title__img" />
+    <img src="https://i0.wp.com/www.citricmagazine.com/wp-content/uploads/2016/08/pokemon-go-1574003_960_720.png?fit=821%2C720&ssl=1&w=640" alt="pokemon silhouette" className="title__img" />
     </div>`;
     document.querySelector("header").innerHTML = header;
   }
   addSection() {
-    const section = `<div class="test">Heyeeeeeeeeeeeee</div>
-<div class="test">Heyeeeeeeeeeeeee</div>
-<div class="test">Heyeeeeeeeeeeeee</div>`;
+    const section = `<section className="main__container">
+    <ul className="main__pokemon-list"></ul>
+</section>`;
     document.querySelector("main").innerHTML = section;
   }
   addFooter() {
-    const footer = `<div class="test">Adiossssssss</div>
-<div class="test">Adiossssssss</div>
-<div class="test">Adiossssssss</div>`;
+    const footer = `<div class="footer__container">
+    <a href="" className="footer__text">made in AlvaroCabero</a>
+</div>`;
     document.querySelector("footer").innerHTML = footer;
   }
 }
