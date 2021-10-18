3f7e646f3bceae9d4a1c89c5637b5f74e853eaac
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-18 04:45:37 +0200

    :construction: Edited Page Class addSection function

--- Page.js
+++ Page.js
@@ -6,39 +6,48 @@ class Page extends Component {
   pokemonList;
   constructor(parentNode) {
     super(parentNode, "pokemon-webpage", "div");
-    this.urlPokeAPI = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
+    this.urlPokeAPI = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=09";
     this.addHeader();
-    this.addSection();
+    this.addSection(this.urlPokeAPI);
     this.addFooter();
+    //this.nextPage();
   }
 
   addHeader() {
     const header = `<div class="title">
-    <h1 class="main-title">------------- WORK IN PROGRESS --------------       :´(  </h1>
+    <h1 class="main-title">POKEMOS <span class="subtitulo">de los Cojones</span></h1>
     <img src="./img/image.png" alt="pokemon silhouette" className="title__img" />
     </div>`;
     document.querySelector(".header").innerHTML = header;
   }
-  addSection() {
+  addSection(url) {
     const section = `<section className="main__container">
     <ul className="main__pokemon-list">
     </ul>
+    <button className="avance">></button>
 </section>`;
 
     document.querySelector(".main").innerHTML = section;
 
     (async () => {
-      const pokemonService = new PokemonServices(this.urlPokeAPI);
+      const pokemonService = new PokemonServices(url);
 
       const pokemonData = await pokemonService.getPokemon();
 
       this.pokemonList = pokemonData.results;
-      console.log(pokemonData.results);
+      //console.log(pokemonData.results);
 
       const tag = document.querySelector("ul");
-      console.log(tag);
+      // console.log(tag);
       this.pokemonList.map((pokemon) => new Pokemon(tag, pokemon.url));
     })();
+  }
+
+  nextPage() {
+    const button = document.querySelector("button");
+
+    this.urlPokeAPI = "https://pokeapi.co/api/v2/pokemon?offset=09&limit=09";
+    button.addEventListener("click", this.addSection(this.urlPokeAPI));
   }
   addFooter() {
     const footer = `<div class="footer__container">
